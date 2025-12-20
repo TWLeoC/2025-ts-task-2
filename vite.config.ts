045c8e6ee2cf41df727a78e7000c00855b023142
LEO CHOU <leotimjack@gmail.com>
2025-12-20 14:05:26 +0800

    change to us gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,37 @@
-import { fileURLToPath, URL } from 'node:url'
+
+import { fileURLToPath, resolve, URL } from 'node:url'
 
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-
+// https://vite.dev/config/
 export default defineConfig({
-  root: fileURLToPath(new URL('.', import.meta.url)),
+  base: '/2025-ts-task-2/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+      output: {
+        dir: resolve(__dirname, 'dist'),
+      },
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
         quietDeps: true,
-        silenceDeprecations: [
-          'mixed-decls',
-          'import',
-          'color-functions',
-          'global-builtin',
-        ],
+        silenceDeprecations: ['mixed-decls', 'import', 'color-functions', 'global-builtin'],
         verbose: false,
       },
     },
   },
-  base: '/2025-ts-task-2/',
 })
